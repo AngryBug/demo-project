6407c449380c6d47bb6b84c92bc3b8f962cb7b06
AngryBug <59223953+AngryBug@users.noreply.github.com>
2021-07-12 16:56:16 +0530

    first commit

---
+++ user-data-service/src/main/java/com/demo/util/ApplicationConstant.java
@@ -0,0 +1,16 @@
+package com.demo.util;
+
+public class ApplicationConstant {
+	
+	
+	public static final String FILE_RESOURCE_PROPERTY_FILE_NAME = "fileresource.properties";
+	
+	public static final String DATA_DELIMETER = "~!";
+	
+	public static final String ROW_DELIMETER = "~#";
+	
+	public static final String PERSONAL_FILE_TYPE = "personal";
+	
+	public static final String ADDRESS_FILE_TYPE = "address";
+	
+}
---
+++ user-data-service/src/main/java/com/demo/util/FileResourceUtility.java
@@ -0,0 +1,33 @@
+package com.demo.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.demo.file.helper.ExcelFileReader;
+import com.demo.file.model.FileInputModel;
+
+public class FileResourceUtility {
+
+	public static FileInputModel getFileDetails(String type) {
+
+		Properties prop = getResourceProperties();
+
+		return new FileInputModel(prop.getProperty(type+".filename"), 
+				prop.getProperty(type+".sheetname"), 
+				prop.getProperty(type+".id"));
+
+	}
+
+	public static Properties getResourceProperties() {
+		Properties prop = new Properties();
+		try(InputStream input = ExcelFileReader.class.getClassLoader().getResourceAsStream("fileresource.properties")){			
+			prop.load(input);						
+		}catch(IOException ioe) {
+			ioe.printStackTrace();
+		}
+		return prop;
+	}
+}
